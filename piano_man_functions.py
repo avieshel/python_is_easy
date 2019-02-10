f457e566a3c1975fe847253d5543a54c8ae30206
Avi Eshel <avi@intranet.venotion.com>
2019-02-10 11:47:43 +0200

    python is easy homework #2 functions

---
+++ piano_man_functions.py
@@ -0,0 +1,24 @@
+# Get the song name as string
+def name():
+    return "Piano Man"
+
+# Get the artist as string
+def artist():
+    return "Billy Joel"
+
+# Get the genre as string
+def genre():
+    return "Soft Rock"
+
+# Was this song released as a single, returns boolean
+def releasedAsSingle():
+    return True
+
+
+print("Name: ", name())
+
+print("Artist: ", artist())
+
+print("Genre: ", genre())
+
+print("This song was released as a single: ", releasedAsSingle())
