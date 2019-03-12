0ad1a731de712c7b961c53901f7a68513aa9e1e7
Avi Eshel <avieshel@gmail.com>
2019-03-12 20:31:57 +0200

    python is easy homework #8 I/O
    Added 'edit' function for extra credit

--- notes.py
+++ notes.py
@@ -1,6 +1,5 @@
 import os
 
-
 # Prompt the user for notes file to use
 notes_file = input('Please enter notes file to use: ')
 
@@ -21,13 +20,13 @@ except FileNotFoundError:
     file_descriptor.close()
 
     # terminate the program
-    exit()
+    quit()
 
 # file did exist, close it and continue with options
 file_descriptor.close()
 
 # prompt user with options
-action = input('Enter "read" if you wish to read the file: ' + notes_file + '\nEnter "delete" if you with to the delete the file: ' + notes_file + '\nEnter "append" if you wish to append another note to the file: ' + notes_file + '\n')
+action = input('Enter "read" if you wish to read the file: ' + notes_file + '\nEnter "delete" if you with to the delete the file: ' + notes_file + '\nEnter "append" if you wish to append another note to the file: ' + notes_file + '\nEnter "edit" if you with to edit a specific line in the file: ' + notes_file + '\n')
 #action = input('read / delete / append?')
 # use this flag to check that a valid action was acted on
 acted_on_action = False
@@ -38,8 +37,9 @@ if (action == "read" and acted_on_action == False):
     # open file for reading (we know it exists)
     file_descriptor = open(file = notes_file, mode = 'r')
     # print the file line by line, note we ommit the default \n from the print statement as it's redundant
-    for line in file_descriptor.readlines():
-        print(line, end='')
+    lines = file_descriptor.readlines()
+    for line in lines:
+        print(line, end = '')
     # close the file
     file_descriptor.close()
 if (action == "delete" and acted_on_action == False):
@@ -60,6 +60,37 @@ if (action == "append" and acted_on_action == False):
     file_descriptor.write(note_to_append)
     # close the file
     file_descriptor.close()
+if (action == 'edit' and acted_on_action == False):
+    # mark the 'edit' action as acted on
+    acted_on_action = True
+    # get the line number from the user as int
+    line_to_edit = int(input('Enter the line you would like to edit: '))
+    # validate that it's value is between 1 and 1000 (this is a random limit, should limit based on MAX_FILE_ZISE)
+    if (1 <= line_to_edit and line_to_edit <= 1000):
+        # get the new note from the user
+        new_data = input('Enter the new text: ')
+        # open the file for writing
+        file_descriptor = open(file=notes_file, mode='r')
+        # read the current contents of the file
+        lines = file_descriptor.readlines()
+        # close the file we just read
+        file_descriptor.close()
+        # edit the line given by the user, consider case when file has 2 lines and the user wants to edit the 5th line (which does not exit)
+        if 1 <= line_to_edit and line_to_edit <= len(lines):
+            # replace the old data with new data, note line index is +1 than list index, don't forget the newline char
+            lines[line_to_edit -1] = new_data + '\n'
+            # open the same file for writing
+            file_descriptor = open(file=notes_file, mode = 'w')
+            # write the edited data
+            file_descriptor.writelines(lines)
+            # close the file
+            file_descriptor.close()
+        else:
+            print('cannot edit line ' + line_to_edit + ' the file you are trying to edit has ' + str(len(lines)) + 'lines')
+    else:
+        print('cannot edit line ' + line_to_edit + ' value must be between 1 and 1000')
+
 if (acted_on_action == False):
     print('Entered option',action,'is not a valid option, please choose between "read", "delete" or "append"')
+
 
